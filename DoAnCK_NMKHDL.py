44a47303687a8c485070352b66f4be0da6397b4e
nttung2603 <tungnguyenthanh2603@gmail.com>
2021-01-06 00:24:53 +0700

    Thu thập dữ liệu, khám phá dữ liệu, huấn luyện mô hình + Xây dựng bộ dữ liệu test cho mô hình huấn luyện

--- DoAnCK_NMKHDL.py
+++ DoAnCK_NMKHDL.py
@@ -13,107 +13,184 @@
 #     name: python3
 # ---
 
-# # Câu hỏi: Tình trạng ô nhiễm ở Thành Phố Hồ Chí Minh trong năm 2020 như thế nào?
+# # Câu hỏi: Có thể phân loại các bài báo không?
 
 # ## Import
 
-import calendar, time;  # Dùng để xử lý dữ liệu thời gian 
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns # seaborn là thư viện được xây trên matplotlib, giúp việc visualization đỡ khổ hơn
 import requests
+from bs4 import BeautifulSoup
+from pyvi import ViTokenizer # thư viện NLP tiếng Việt
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
 import pandas as pd
 
-
 # ---
 
-# # Thu thập dữ liệu từ web OpenWeatherMap dùng API
+# ## Thu thập dữ liệu
 
-# Đầu tiên, em sẽ viết hàm `collect_data` ở bên dưới. Hàm này có các input:
-# - `lat` và `lon`: là số, cho biết tọa độ địa lý của vị trí đang xét (vĩ độ, tung độ). Ví dụ: `lat=50`
-# - `start`, `end`: là chuỗi, cho biết khoảng thời gian muốn lấy dữ liệu. Sau khi truyền vào `start` và `end` sẽ được xử lý chuyển sang epoch timestamp. 
-# - `GMT`: là một số, thể hiện muối giờ của vị trí đang xét.
+# Lưu các ký tự đặc biệt trong tiếng Việt vào biến `list_symbols`
+
+list_symbols = ['\n']
+def get_symbols(start, end):
+    list_symbols = []
+    for i in range(start, end + 1):
+        list_symbols.append(chr(i))
+    return list_symbols
+list_symbols.extend(get_symbols(33, 47))
+list_symbols.extend(get_symbols(58, 64))
+list_symbols.extend(get_symbols(33, 47))
+list_symbols.extend(get_symbols(91, 96))
+list_symbols.extend(get_symbols(123, 126))
+
+# Biến `subjects` lưu chủ đề của các bài báo
+
+# subjects = ['thoi-su', 'the-gioi', 'kinh-doanh', 'giai-tri',
+#             'the-thao', 'giao-duc', 'suc-khoe', 'du-lich', 'khoa-hoc']
+subjects = ['thoi-su','the-gioi','kinh-doanh', 'giai-tri', 'the-thao', 'giao-duc', 'suc-khoe', 'du-lich']
+
+# Hàm `get_urls` ở bên dưới có các input:
+# - `subject`: là một chuỗi, thể hiện chủ đề của bài báo ta muốn tìm kiếm.
+# - `page`: là một số, thể hiện trang mà ta muốn lấy dữ liệu
+# Output: trả về list các url của `subject` trong `page` đó.
 #
-# Ví dụ, `start='2020-11-24 13:16:42'` `GMT=7` sẽ được chuyển thành `start_epoch=1606223802`.
-# - `API_key`: là chuỗi, cho biết API key của bạn. Ví dụ API key của mình là `appid=3aefc892a62fabd9fd044500931328db`.
+# Ví dụ một đường link lấy url: https://vnexpress.net/thoi-su-p1
 #
-# Trong hàm này bạn sẽ vào đường link `f'http://api.openweathermap.org/data/2.5/air_pollution/history?lat={lat}&lon={lon}&start={start_epoch}&end={end_epoch}&appid={API key}'` (dạng f-string trong Python) để thu thập dữ liệu. 
+# <b>Nhưng</b> ở chủ đề `the-thao` do cấu trúc của trang web nên url sẽ thay đổi. VD: https://vnexpress.net/the-thao/p1
+
+# +
+articles = dict()
+def get_urls(subject, page):
+    urls = []
+    url = f"https://vnexpress.net/{subject}-p{page}"
+    response = requests.get(url)
+    if response.url == 'https://vnexpress.net/error.html':
+        url = f"https://vnexpress.net/{subject}/p{page}"
+    response = requests.get(url)
+    html_text = response.text
+    tree = BeautifulSoup(html_text, 'html.parser')
+    titles = tree.find_all('h2', {'class': 'title-news'})
+    for i in range(len(titles)):
+        urls.append(titles[i].a["href"])
+    titles = tree.find_all('h3', {'class': 'title-news'})
+    for i in range(len(titles)):
+        urls.append(titles[i].a["href"])
+    return urls
+            
+def get_datas(subjects, num_page = 1):
+    articles = dict()
+    for subject in subjects:
+        contents = []
+        for page in range(1, num_page + 1):
+            urls = get_urls(subject, page)
+            for url in urls:
+                print(url)
+                html_text = requests.get(url).text
+                tree = BeautifulSoup(html_text, 'html.parser')
+                content = tree.find('div', {'class': 'sidebar-1'})
+                header = content.find('div', {'class': 'header-content width_common'})
+                if (header != None):
+                    header.decompose()
+                """writer = content.find('p', {'class': 'Normal'})
+                if (writer['style'] != None):
+                    list_news.decompose()"""
+                list_news = content.find('ul', {'class': 'list-news'})
+                if (list_news != None):
+                    list_news.decompose()
+                footer = content.find('div', {'class': 'footer-content'})
+                if (footer != None):
+                    footer.decompose()
+                text = content.text
+                for symbol in list_symbols:
+                    text = text.replace(symbol, ' ')
+
+                contents.append(text)
+        articles[subject] = contents
+    return articles
+
+
+# -
+
+# Hàm `get_datas` ở bên dưới có các input:
+# - `subjects`: là một list các chủ đề.
+# - `num_page`: là một số, thể hiện mỗi chủ đề sẽ tìm kiếm bao nhiêu trang.
 #
-# Ví dụ, bạn có thể vào thử ở web browser: http://api.openweathermap.org/data/2.5/air_pollution/history?lat=50&lon=50&start=1606223802&end=1606482999&appid=3aefc892a62fabd9fd044500931328db.
+# Output: Trả về dictionary với key là chủ đề bài báo, value: là một list nội dung các bài báo trong chủ đề đó.
+#
+# Trong hàm ta sẽ xử lý một phần dữ liệu: Xóa đi các ký tự đặc biệt trong đoạn text ta thu thập được.
 
-def collect_data(lat, lon, start, end, API_key, GMT=7):
-    start_epoch = calendar.timegm(time.strptime(start, '%Y-%m-%d %H:%M:%S')) - GMT*3600
-    end_epoch = calendar.timegm(time.strptime(end, '%Y-%m-%d %H:%M:%S')) - GMT*3600
-    url = f'http://api.openweathermap.org/data/2.5/air_pollution/history?lat={lat}&lon={lon}&start={start_epoch}&end={end_epoch}&appid={API_key}'
-    r = requests.get(url)
-    json_data = None
-    if r.ok:
-        r = requests.get(url)
-        json_data = r.json()
-    return json_data
+def get_datas(subjects, num_page = 1):
+    articles = dict()
+    for subject in subjects:
+        contents = []
+        for page in range(1, num_page + 1):
+            urls = get_urls(subject, page)
+            for url in urls:
+                #print(url)
+                html_text = requests.get(url).text
+                tree = BeautifulSoup(html_text, 'html.parser')
+                content = tree.find('div', {'class': 'sidebar-1'})
+                header = content.find('div', {'class': 'header-content width_common'})
+                if (header != None):
+                    header.decompose()
+                """writer = content.find('p', {'class': 'Normal'})
+                if (writer['style'] != None):
+                    list_news.decompose()"""
+                list_news = content.find('ul', {'class': 'list-news'})
+                if (list_news != None):
+                    list_news.decompose()
+                footer = content.find('div', {'class': 'footer-content'})
+                if (footer != None):
+                    footer.decompose()
+                text = content.text
+                for symbol in list_symbols:
+                    text = text.replace(symbol, ' ')
 
-
-# Tạo DataFrame từ json_data thu thập được. Em sẽ tạo các cột:
-# - Air Quality Index: Mức độ ô nhiễm. Các giá trị có thể: 1, 2, 3, 4, 5. Trong đó: 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor.
-# - CO: Hàm lượng khí CO (Carbon monoxide), μg/m3.
-# - NO: Hàm lượng khí NO (Nitrogen monoxide), μg/m3.
-# - NO2: Hàm lượng khí NO2 (Nitrogen dioxide), μg/m3.
-# - O3: Hàm lượng khí O3 (Ozone), μg/m3.
-# - SO2: Hàm lượng khí SO2 (Sulphur dioxide), μg/m3.
-# - PM2_5: Hàm lượng khí PM2.5 (Fine particles matter), μg/m3.
-# - PM10: Hàm lượng khí PM10 (Coarse particulate matter), μg/m3.
-# - NH3: Hàm lượng khí NH3 (Ammonia), μg/m3.
-# - DateTime: Thời điểm thu thập dữ liệu ('%Y-%m-%d %H:%M:%S').
-
-def create_dataframe(json_data):
-    rows = {'Air Quality Index':[],
-            'co': [],
-            'no': [],
-            'no2': [],
-            'o3': [],
-            'so2': [],
-            'pm2_5': [],
-            'pm10': [],
-            'nh3': [],
-            'DateTime': []}
-    for data in json_data['list']:
-        aqi = data['main']['aqi']
-        rows['Air Quality Index'].append(aqi)
-        #-----
-        components = data['components']
-        for key in components.keys():
-            rows[key].append(components[key])
-         #---- 
-        dt_epoch = data['dt']
-        rows['DateTime'].append(dt_epoch)
-    df = pd.DataFrame(rows)
-    df.sort_values(by='DateTime', inplace = True, ignore_index = True)
-    return df
+                contents.append(text)
+        articles[subject] = contents
+    return articles
 
 
-# ---
-
-# ### TEST (Sau này xóa)
-
-#Địa chỉ thành phố Hồ Chí Minh
-lat = 10.8
-lon = 106.7
-start = '2020-1-1 0:0:0'#1/1/2020
-end = '2020-12-31 23:59:59' #31/1/2020
-API_key = '3aefc892a62fabd9fd044500931328db'
 #Thu thập dữ liệu
-json_data = collect_data(lat, lon, start, end, API_key)
-#Tạo data frame
-data_df = create_dataframe(json_data)
-data_df
+articles =  get_datas(subjects, num_page = 2)
 
-# ---
 
-# ## Tiền xử lý
+# Hàm `split_word` ở bên dưới có input:
+# - `articles`: là một dictionary với key là chủ đề bài báo, value: là một list nội dung các bài báo trong chủ đề đó.
+#
+# Output: Trả về biến `articles` sau khi đã tách các từ trong văn bản
 
-# Chuyển cột Date Time về dạng `%Y-%m-%d %H:%M:%S`
+def split_word(articles):
+    for subject in articles:
+        for i in range(len(articles[subject])):
+            articles[subject][i] = ViTokenizer.tokenize(articles[subject][i])
+    return articles
+articles = split_word(articles)
 
-for i in range(len(data_df)):
-    x = data_df['DateTime'].iloc[i]
-    data_df['DateTime'].iloc[i] = time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime(x))
-data_df
+
+# Hàm `split_word` ở bên dưới có input:
+# - `articles`: là một dictionary với key là chủ đề bài báo, value: là một list nội dung các bài báo trong chủ đề đó.
+#
+# Output: Trả về DataFrame được tạo ra từ `articles`.
+
+def create_dataframe(articles):
+    data = []
+    for subject in articles:
+        temp_data = []
+        temp_data.append(articles[subject])
+        temp_data.append(subject)
+        data.append(temp_data)
+    data_df = pd.DataFrame(data, columns=['Nội dung văn bản', 'Chủ đề'])
+    data_df = data_df.explode('Nội dung văn bản', ignore_index = True)
+    return data_df
+data_df = create_dataframe(articles)
 
 # ---
 
@@ -121,25 +198,118 @@ data_df
 
 data_df.head()
 
-# ### Dữ liệu có bao nhiêu dòng và bao nhiêu cột?
-
+#Dữ liệu có bao nhiêu dòng và bao nhiêu cột?
 data_df.shape
 
-# ### Dữ liệu có các dòng bị lặp không?
-
+# Dữ liệu có các dòng bị lặp không?
 data_df.index.duplicated().sum()
 
-# ## Khám phá dữ liệu (để biết cách tách các tập)
-# Để biết cách tách các tập thì ta cần khám phá thêm cột output một ít:
-# - Cột này hiện có kiểu dữ liệu là gì? Trong bài toán hồi qui thì cột output bắt buộc phải có dạng số; nếu hiện chưa có dạng số (ví dụ, số nhưng được lưu dưới dạng chuỗi) thì ta cần chuyển sang dạng số rồi mới tách các tập.
-# - Cột này có giá trị thiếu không? Nếu có giá trị thiếu thì ta sẽ bỏ các dòng mà output có giá trị thiếu rồi mới tách các tập (loại học mà học từ dữ liệu trong đó output có giá trị thiếu được gọi là semi-supervised learning; trong phạm vi môn học, ta không đụng tới loại học này).
-# - Nếu cột này có dạng categorical (phân lớp) thì tỉ lệ các lớp như thế nào? Nếu tỉ lệ các lớp bị chênh lệch nhau quá nhiều thì có thể ta sẽ cần qua lại bước thu thập dữ liệu và thu thập thêm để cho tỉ lệ các lớp không bị chênh lệnh quá nhiều (hoặc khi đánh giá ta cần có một độ lỗi phù hợp).
-
-# Cột output hiện có kiểu dữ liệu gì?
-data_df['Air Quality Index'].dtype
+#Cột output hiện có kiểu dữ liệu gì?
+data_df['Nội dung văn bản'].dtypes
 
 # Cột output có giá trị thiếu không?
-data_df['Air Quality Index'].isna().sum()
+data_df['Chủ đề'].isna().sum()
 
 # Tỉ lệ các lớp trong cột output?
-data_df['Air Quality Index'].value_counts(normalize=True) * 100
+data_df['Chủ đề'].value_counts(normalize=True) * 100
+
+# Nhận xét: Tập dữ liệu phân bố khá đều.
+
+# ---
+
+# ## Tách dữ liệu + Mô hình hóa
+
+#Tách dữ liệu thành tập train và tập test
+y_df = data_df['Chủ đề']
+X_df = data_df['Nội dung văn bản']
+X_train, X_val, y_train, y_val = train_test_split(X_df, y_df, test_size=0.3,stratify = y_df, random_state=0)
+
+# Chuyển đổi Output từ dạng chuỗi thành dạng số
+
+encoder = preprocessing.LabelEncoder()
+y_train= encoder.fit_transform(y_train)
+y_val = encoder.transform(y_val)
+
+# Tạo Pipeline
+
+process_pipeline = Pipeline([('tfidf',TfidfVectorizer(analyzer='word', max_features=15000, ngram_range=(2, 3))),
+                             ('classifier',MultinomialNB(alpha = 0.5))])
+
+# ### Tìm mô hình tốt nhất
+
+# Tìm giá trị tốt nhất của hai siêu tham số `alpha` và `max_features`
+
+train_errs = []
+val_errs = []
+alphas = [0.1, 0.5, 1, 1.5]
+max_features_s = [5000, 10000, 15000, 20000, 25000]
+best_val_err = float('inf'); 
+best_alpha = None; 
+best_max_features = None
+for alpha in alphas:
+    for max_features in max_features_s:
+        process_pipeline.set_params(tfidf__max_features = max_features, 
+                                    classifier__alpha = alpha)
+        process_pipeline.fit(X_train, y_train)
+        train_score = (1 - process_pipeline.score(X_train, y_train))*100
+        val_score = (1 - process_pipeline.score(X_val, y_val))*100
+        train_errs.append(train_score)
+        val_errs.append(val_score)
+        if float(val_score) < best_val_err:
+            best_val_err = val_score
+            best_alpha = alpha
+            best_max_features = max_features
+    
+
+# Trực quan hóa kết quả
+train_errs_df = pd.DataFrame(data=np.array(train_errs).reshape(len(alphas), -1),
+                             index=alphas, columns=max_features_s)
+val_errs_df = pd.DataFrame(data=np.array(val_errs).reshape(len(alphas), -1), 
+                           index=alphas, columns=max_features_s)
+min_err = min(min(train_errs), min(val_errs))
+max_err = max(max(train_errs), max(val_errs))
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+sns.heatmap(train_errs_df, vmin=min_err, vmax=max_err, square=True, annot=True, 
+            cbar=False, fmt='.1f', cmap='Reds')
+plt.title('train errors'); plt.xlabel('max_features'); plt.ylabel('alpha')
+plt.subplot(1, 2, 2)
+sns.heatmap(val_errs_df, vmin=min_err, vmax=max_err, square=True, annot=True, 
+            cbar=False, fmt='.1f', cmap='Reds')
+plt.title('validation errors'); plt.xlabel('max_features'); plt.ylabel('alpha');
+
+# Lấy `best_alpha` và `best_max_features` để huấn luyện mô hình
+
+process_pipeline.set_params(tfidf__max_features = best_max_features, 
+                                    classifier__alpha = best_alpha)
+process_pipeline.fit(X_train, y_train)
+
+# Kết quả dự đoán tập huấn luyện
+
+train_predictions = process_pipeline.predict(X_train)
+accuracy_score(train_predictions, y_train)
+
+# Kết quả dự đoán tập validation
+
+val_predictions = process_pipeline.predict(X_val)
+accuracy_score(val_predictions, y_val)
+
+# ### Mô hình tốt nhất
+
+process_pipeline.set_params(tfidf__max_features = best_max_features, 
+                                    classifier__alpha = best_alpha)
+y_df = encoder.transform(y_df)
+process_pipeline.fit(X_df, y_df)
+
+# ### Sử dụng mô hình tốt nhất để dự đoán tập test
+
+# Dữ liệu test được lấy từ trang web https://vietnamnet.vn/
+#
+# Code thu thập dữ liệu test và lưu vào file `test.csv` xem trên github: `collect_test.ipynb`
+
+test_df = pd.read_csv('test.csv', index_col = 0)
+test_y_df = data_df['Chủ đề']
+test_X_df = data_df['Nội dung văn bản']
+test_y_df = encoder.transform(test_y_df)
+test_predictions = process_pipeline.predict(test_X_df)
+accuracy_score(test_predictions, test_y_df)
