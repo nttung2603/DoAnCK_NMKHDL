dc13778dcaf35f71cfa69894a288e236f10cdd19
nttung2603 <tungnguyenthanh2603@gmail.com>
2021-01-14 06:36:49 +0700

    Cap nhat lai file data, test, DoAnCK_NMKHDL. Chinh collect_test thanh collect_data. Them slide thuyet trinh

--- DoAnCK_NMKHDL.py
+++ DoAnCK_NMKHDL.py
@@ -21,141 +21,45 @@
 import matplotlib.pyplot as plt
 import seaborn as sns # seaborn là thư viện được xây trên matplotlib, giúp việc visualization đỡ khổ hơn
 import requests
+import numpy as np
+import pandas as pd
+import time # để sleep chương trình
 from bs4 import BeautifulSoup
 from pyvi import ViTokenizer # thư viện NLP tiếng Việt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
-import pandas as pd
+#Mô hình phân lớp
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import MultinomialNB
+
 
 # ---
 
 # ## Thu thập dữ liệu
-
-# Lưu các ký tự đặc biệt trong tiếng Việt vào biến `list_symbols`
-
-list_symbols = ['\n']
-def get_symbols(start, end):
-    list_symbols = []
-    for i in range(start, end + 1):
-        list_symbols.append(chr(i))
-    return list_symbols
-list_symbols.extend(get_symbols(33, 47))
-list_symbols.extend(get_symbols(58, 64))
-list_symbols.extend(get_symbols(33, 47))
-list_symbols.extend(get_symbols(91, 96))
-list_symbols.extend(get_symbols(123, 126))
-
-# Biến `subjects` lưu chủ đề của các bài báo
-
-# subjects = ['thoi-su', 'the-gioi', 'kinh-doanh', 'giai-tri',
-#             'the-thao', 'giao-duc', 'suc-khoe', 'du-lich']
-subjects = ['thoi-su','the-gioi','kinh-doanh', 'giai-tri', 'the-thao', 'giao-duc', 'suc-khoe', 'du-lich']
-
-# Hàm `get_urls` ở bên dưới có các input:
-# - `subject`: là một chuỗi, thể hiện chủ đề của bài báo ta muốn tìm kiếm.
-# - `page`: là một số, thể hiện trang mà ta muốn lấy dữ liệu
-# Output: trả về list các url của `subject` trong `page` đó.
-#
-# Ví dụ một đường link lấy url: https://vnexpress.net/thoi-su-p1
-#
-# <b>Nhưng</b> ở chủ đề `the-thao` do cấu trúc của trang web nên url sẽ thay đổi. VD: https://vnexpress.net/the-thao/p1
-
-articles = dict()
-def get_urls(subject, page):
-    urls = []
-    url = f"https://vnexpress.net/{subject}-p{page}"
-    response = requests.get(url)
-    if response.url == 'https://vnexpress.net/error.html':
-        url = f"https://vnexpress.net/{subject}/p{page}"
-    response = requests.get(url)
-    html_text = response.text
-    tree = BeautifulSoup(html_text, 'html.parser')
-    titles = tree.find_all('h2', {'class': 'title-news'})
-    for i in range(len(titles)):
-        urls.append(titles[i].a["href"])
-    titles = tree.find_all('h3', {'class': 'title-news'})
-    for i in range(len(titles)):
-        urls.append(titles[i].a["href"])
-    return urls
-
-
-# Hàm `get_data` ở bên dưới có các input:
-# - `subjects`: là một list các chủ đề.
-# - `num_page`: là một số, thể hiện mỗi chủ đề sẽ tìm kiếm bao nhiêu trang.
-#
-# Output: Trả về dictionary với key là chủ đề bài báo, value: là một list nội dung các bài báo trong chủ đề đó.
-#
-# Trong hàm ta sẽ xử lý một phần dữ liệu: Xóa đi các ký tự đặc biệt trong đoạn text ta thu thập được.
-
-def get_data(subjects, num_page = 1):
-    articles = dict()
-    for subject in subjects:
-        contents = []
-        for page in range(1, num_page + 1):
-            urls = get_urls(subject, page)
-            for url in urls:
-                #print(url)
-                html_text = requests.get(url).text
-                tree = BeautifulSoup(html_text, 'html.parser')
-                content = tree.find('div', {'class': 'sidebar-1'})
-                header = content.find('div', {'class': 'header-content width_common'})
-                if (header != None):
-                    header.decompose()
-                """writer = content.find('p', {'class': 'Normal'})
-                if (writer['style'] != None):
-                    list_news.decompose()"""
-                list_news = content.find('ul', {'class': 'list-news'})
-                if (list_news != None):
-                    list_news.decompose()
-                footer = content.find('div', {'class': 'footer-content'})
-                if (footer != None):
-                    footer.decompose()
-                text = content.text
-                for symbol in list_symbols:
-                    text = text.replace(symbol, ' ')
-
-                contents.append(text)
-        articles[subject] = contents
-    return articles
-
-
-#Thu thập dữ liệu
-articles =  get_data(subjects, num_page = 2)
-
 
 # Hàm `split_word` ở bên dưới có input:
 # - `articles`: là một dictionary với key là chủ đề bài báo, value: là một list nội dung các bài báo trong chủ đề đó.
 #
 # Output: Trả về biến `articles` sau khi đã tách các từ trong văn bản
 
-def split_word(articles):
-    for subject in articles:
-        for i in range(len(articles[subject])):
-            articles[subject][i] = ViTokenizer.tokenize(articles[subject][i])
-    return articles
-articles = split_word(articles)
+def split_word(df):
+    for i in range(len(df['Nội dung văn bản'])):
+        df['Nội dung văn bản'][i] =  ViTokenizer.tokenize(df['Nội dung văn bản'][i])
+    return df
 
 
-# Hàm `create_dataframe` ở bên dưới có input:
-# - `articles`: là một dictionary với key là chủ đề bài báo, value: là một list nội dung các bài báo trong chủ đề đó.
-#
-# Output: Trả về DataFrame được tạo ra từ `articles`.
+# Đầu tiên ta sẽ lấy dữ liệu đã thu thập được từ file `data.csv`
 
-def create_dataframe(articles):
-    data = []
-    for subject in articles:
-        temp_data = []
-        temp_data.append(articles[subject])
-        temp_data.append(subject)
-        data.append(temp_data)
-    data_df = pd.DataFrame(data, columns=['Nội dung văn bản', 'Chủ đề'])
-    data_df = data_df.explode('Nội dung văn bản', ignore_index = True)
-    return data_df
-data_df = create_dataframe(articles)
+data_df = pd.read_csv('data.csv', index_col = 0)
+data_df
+
+# Sau đó ta thực hiện tách từ trong dữ liệu bằng hàm `split_word`
+
+data_df = split_word(data_df)
 data_df
 
 # ---
@@ -170,7 +74,7 @@ data_df.shape
 # Dữ liệu có các dòng bị lặp không?
 data_df.index.duplicated().sum()
 
-#Cột output hiện có kiểu dữ liệu gì?
+#Kiểu dữ liệu của các cột
 data_df.dtypes
 
 #Có dòng nào không lấy được nội dung văn bản không?
@@ -188,23 +92,24 @@ data_df['Chủ đề'].value_counts(normalize=True) * 100
 
 # ## Tiền xử lý dữ liệu
 
-# Xóa đi những dòng có số kí tự ít hơn `?` (Có thể có những dòng không lấy được dữ liệu vì không giống định dạng mẫu)
+# Xóa đi những dòng có số kí tự ít hơn 100 (Có thể có những dòng không lấy được dữ liệu vì không giống định dạng mẫu)
 
-data_df.drop(data_df[data_df['Nội dung văn bản'].map(len) < 500].index, inplace = True)
+data_df.drop(data_df[data_df['Nội dung văn bản'].map(len) < 100].index, inplace = True)
 #Dữ liệu sau khi xóa.
 data_df.shape
 
 # Ta sẽ chuyển cột Output `Chủ đề` thành dạng số
 
-y_df = data_df['Chủ đề']
-X_df = data_df['Nội dung văn bản']
 encoder = LabelEncoder()
-y_df = encoder.fit_transform(y_df)
+data_df['Chủ đề'] = encoder.fit_transform(data_df['Chủ đề'])
+data_df
 
 # ---
 
 # ## Tách dữ liệu + Mô hình hóa
 
+X_df = data_df['Nội dung văn bản']
+y_df = data_df['Chủ đề']
 #Tách dữ liệu thành tập train và tập test
 X_train, X_val, y_train, y_val = train_test_split(X_df, y_df, test_size=0.3,stratify = y_df, random_state=0)
 
@@ -219,8 +124,8 @@ process_pipeline = Pipeline([('tfidf',TfidfVectorizer(analyzer='word', max_featu
 
 train_errs = []
 val_errs = []
-alphas = [0.1, 0.5, 1, 1.5]
-max_features_s = [5000, 10000, 15000, 20000, 25000]
+alphas = [0.05, 0.1, 0.5, 1]
+max_features_s = [10000, 15000, 20000, 25000, 30000]
 best_val_err = float('inf'); 
 best_alpha = None; 
 best_max_features = None
@@ -259,7 +164,7 @@ plt.title('validation errors'); plt.xlabel('max_features'); plt.ylabel('alpha');
 # Lấy `best_alpha` và `best_max_features` để huấn luyện mô hình
 
 process_pipeline.set_params(tfidf__max_features = best_max_features, 
-                                    classifier__alpha = best_alpha)
+                            classifier__alpha = best_alpha)
 process_pipeline.fit(X_train, y_train)
 
 # Kết quả dự đoán tập huấn luyện
@@ -276,17 +181,30 @@ accuracy_score(val_predictions, y_val)
 
 process_pipeline.set_params(tfidf__max_features = best_max_features, 
                             classifier__alpha = best_alpha)
-process_pipeline.fit(X_df, y_df)
 
 # ### Sử dụng mô hình tốt nhất để dự đoán tập test
 
 # Dữ liệu test được lấy từ trang web https://vietnamnet.vn/
-#
-# Code thu thập dữ liệu test và lưu vào file `test.csv` xem trên github: `collect_test.ipynb`
 
-test_df = pd.read_csv('test.csv', index_col = 0)
-test_y_df = data_df['Chủ đề']
-test_X_df = data_df['Nội dung văn bản']
+# Đầu tiên ta lấy dữ liệu tập test đã thu thập được từ file `test.csv`
+
+data_test_df = pd.read_csv('test.csv', index_col = 0)
+data_test_df
+
+# Sau đó ta thực hiện tách từ trong dữ liệu bằng hàm `split_word` tương tự như ở trên
+
+data_test_df = split_word(data_test_df)
+data_test_df
+
+# Xóa đi những dòng có số kí tự ít hơn 100 (Có thể có những dòng không lấy được dữ liệu vì không giống định dạng mẫu)
+
+data_test_df.drop(data_test_df[data_test_df['Nội dung văn bản'].map(len) < 100].index, inplace = True)
+data_test_df.shape
+
+# Cuối cùng ta dự đoán kết quả tập dữ liệu test và tính tỉ lệ chính xác
+
+test_X_df = data_test_df['Nội dung văn bản']
+test_y_df = data_test_df['Chủ đề']
 test_y_df = encoder.transform(test_y_df)
 test_predictions = process_pipeline.predict(test_X_df)
 accuracy_score(test_predictions, test_y_df)
